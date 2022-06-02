a37376c4cf2bca4b32d29942be456581b466743b
init <44498727+initt5@users.noreply.github.com>
2022-06-02 20:58:12 +0200

    Add titles to the bot message

--- bot.py
+++ bot.py
@@ -9,10 +9,21 @@ load_dotenv()
 client = discord.Client()
 
 
+def get_data(link):
+    data = {}
+    if link.startswith('/d/'):
+        data['description'] = f"[OLX](https://www.olx.pl{link})"
+        pre_title = link.split('/')[3]
+        data['title'] = ' '.join(pre_title.split('-')[:-2]).capitalize()
+    else:
+        data['description'] = f"[OLX]({link})"
+        pre_title = link.split('/')[5]
+        print(pre_title)
+        data['title'] = ' '.join(pre_title.split('-')[:-1]).capitalize()
+    return data
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
 
 @client.event
 async def on_message(message):
@@ -24,8 +35,9 @@ async def on_message(message):
         links = flat_list.get_list_of_flats()
         embed = discord.Embed()
         for link in links:
-            embed = discord.Embed(title="Sample Embed",
-                                  description=f"[OLX](https://www.olx.pl/d/oferta/{link})",
+            data = get_data(link)
+            embed = discord.Embed(title=data['title'],
+                                  description=data['description'],
                                   color=0xFF5733)
             await message.channel.send(embed=embed)
 
--- scrap.py
+++ scrap.py
@@ -7,24 +7,15 @@ class FlatList:
         self.URL = "https://www.olx.pl/d/nieruchomosci/mieszkania/wynajem/warszawa/?search%5Border%5D=created_at:desc"
         self.page = requests.get(self.URL)
         self.soup = BeautifulSoup(self.page.content, "html.parser")
+        self.url_list = []
 
     def get_list_of_flats(self):
         results = self.soup.find_all("a", {"class": "css-1bbgabe"})
         url_list = []
         for result in results:
             link_url = result['href']
-            cleared_link = self.clear_link(link_url)
-            url_list.append(cleared_link)
+            url_list.append(link_url)
         return url_list
-
-    def clear_link(self, link):
-        i = 0
-        if "otodom" in link:
-            i = 5
-        else:
-            i = 3
-        good_link = link.split('/')[i]
-        return good_link
 
 
 flats = FlatList()
